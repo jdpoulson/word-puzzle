ee27555ccb4e345f3bd48a509d38a556f866eb79
John Poulson <Jdpoulson@gmail.com>
2019-02-06 13:54:19 -0800

    added changes to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 var vowels = ['a', 'e', 'i', 'o', 'u'];
-var sentence = [];
 $(document).ready(function() {
   $("form#word").submit(function(event) {
     var input = $("input#sentence").val();
-    sentence = input.split('');
-    console.log(sentence)
+    var sentences = input.split('');
+    for (var x = 0; x < sentences.length; x += 1) {
+      vowels.forEach(function(vowel) {
+        if (sentences[x] === vowel) {
+          sentences[x] = '-'
+        }
+      })
+    }
+    var sentences2 = sentences.join('')
+    $("#mysentence").append("<p>" + sentences2 + "</p>")
+    $("#mysentence").show();
     // for (var words = 0; words < vowels.split(); words += 1) {
     //   sentence += vowels[words];
     // };
     event.preventDefault();
-
   });
 });
